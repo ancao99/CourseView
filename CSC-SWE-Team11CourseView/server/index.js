7cfcc155088f91fa4012adb9f077b2f51fa72b98
Kush <islayusim@live.com>
2024-04-14 15:28:36 -0400

    ...

--- CSC-SWE-Team11CourseView/server/index.js
+++ CSC-SWE-Team11CourseView/server/index.js
@@ -10,10 +10,6 @@ import { dirname } from 'path';
 import path from "path";
 import Authentication from "./behavior/authentication.js";
 import Profile from "./behavior/profile.js";
-import User from "./behavior/user.js";
-import Courses from "./behavior/courses.js";
-import Feedback from "./behavior/feedback.js";
-import Review from "./behavior/review.js";
 
 // Server port
 var app = express()
@@ -29,19 +25,19 @@ checkDatabase();
 // read picture from disk
 const getImageData = (folder, imageName) => {
     const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
+    const __dirname = dirname(__filename);    
     let imagePath = path.join(__dirname, folder, imageName);
     try {
         // Read the contents of the image file
         const imageData = fs.readFileSync(imagePath);
         return imageData;
-    } catch (error) {
-        try {
+    } catch (error) {        
+        try{
             imagePath = path.join(__dirname, 'images', "not-found.png");
             const imageData2 = fs.readFileSync(imagePath);
             return imageData2;
         }
-        catch (err) {
+        catch(err){
             console.error('Error reading image file:', error);
         }
         return null;
@@ -55,17 +51,17 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         const list = file.originalname.split('.');
-        cb(null, `${Date.now()}.${list[list.length - 1]}`); // Set your filename logic
+        cb(null, `${Date.now()}.${list[list.length-1]}`); // Set your filename logic
     },
 });
-const upload = multer({ storage: storage });
+const upload = multer({storage: storage});
 
 // Switcher
-app.post("/dummydata", upload.single('picture'), async (req, res) => {
-    if (req === null) return res.status(400).json("Bad Request");
-
+app.post("/dummydata", upload.single('picture'), async (req, res) => {  
+    if (req === null) return res.status(400).json("Bad Request");  
+    
     let data = JSON.parse(req.body.data);
-    let key = req.body.key;
+    let key = req.body.key;  
     switch (data.action) {
         // authentication
         case "login":
@@ -82,88 +78,23 @@ app.post("/dummydata", upload.single('picture'), async (req, res) => {
             break;
 
         //profile
-        case "updateProfile":
-            Profile.updateProfile(data.entry, res);
+        case "updateUser":
+            Profile.updateUser(data.entry, res);
             break;
-        case "deleteProfile":
-            Profile.deleteProfile(data.entry, res);
-            break;
-
-        //client
-        case "deleteClient":
-            User.deleteClient(data.entry, res);
-            break;
-        case "getClient":
-            User.getClient(key, data.entry, res);
-            break;
-        case "updateClient":
-            User.updateClient(data.entry, res);
-            break;
-        case "getClientDetail":
-            console.log("i got call")
-            User.getClientDetail(key, data.entry, res);
+        case "deleteUser":
+            Profile.deleteUser(data.entry, res);
             break;
 
-        //courses
-        case "getCourses":
-            Courses.getCourses(key, data.entry, res);
-            break;
-        case "deleteCourses":
-            Courses.deleteCourses(data.entry, res);
-            break;
-        case "addCourses":
-            Courses.addCourses(data.entry, res);
-            break;
-        case "updateCourses":
-            Courses.updateCourses(data.entry, res);
-            break;
-        case "getCoursesDetail":
-            Courses.getCoursesDetail(key, data.entry, res);
-            break;
-
-        //feedback
-        case "getFeedback":
-            Feedback.getFeedback(key, data.entry, res);
-            break;
-        case "deleteFeedback":
-            Feedback.deleteFeedback(data.entry, res);
-            break;
-        case "addFeedback":
-            Feedback.addFeedback(data.entry, res);
-            break;
-        case "updateFeedback":
-            Feedback.updateFeedback(data.entry, res);
-            console.log(data.entry)
-            break;
-
-        //review
-        case "getComments":
-            Review.getComments(key, data.entry, res);
-            break;
-        case "addComments":
-            Review.addComments(data.entry, res);
-            break;
-        case "deleteComments":
-            Review.deleteComments(data.entry, res);
-            break;
-        case "updateComments":
-            Review.updateComments(data.entry, res);
-            break;
-        case "getCommentsDetail":
-            Review.getCommentsDetail(key, data.entry, res);
-            break;
-
-        default:
+        default:        
             res.status(400).json("Bad Request");
             break;
-
     }
 })
 
 // send picture out
 app.get('/dummydata/:imageName', (req, res) => {
     const imageName = req.params.imageName;
-    const imageData = getImageData('images', imageName);
+    const imageData = getImageData('images',imageName);
 
     // Set appropriate headers
     res.setHeader('Content-Type', 'image/jpeg');
@@ -176,7 +107,7 @@ app.get('/dummydata/:imageName', (req, res) => {
 app.get('/dummydata/default/:imageName', (req, res) => {
     //console.log(req.params)
     const imageName = req.params.imageName;
-    const imageData = getImageData('default', imageName);
+    const imageData = getImageData('default',imageName);
 
     // Set appropriate headers
     res.setHeader('Content-Type', 'image/jpeg');
@@ -186,6 +117,6 @@ app.get('/dummydata/default/:imageName', (req, res) => {
     res.end(imageData);
 });
 
-app.listen(port, () => {
+app.listen(port, ()=> {
     console.log(`app listening at http://localhost:${port}`)
 })
